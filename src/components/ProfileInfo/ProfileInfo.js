4669a32a167ffde7a64390870d8beaa783a542de
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-03-21 15:00:51 +0100

    added info container in profileinfo + beginning of responsive

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -24,11 +24,11 @@ const ProfileInfo = () => {
                                 </div>
                                 <div className="profile-info-container image profile-picture overlay gradient-group name-connected-distance">
                                     <div className="profile-info-container image profile-picture overlay gradient-group name-connected-distance name-connected-group">
-                                        <h3>Name, age</h3>
+                                        <h2>Name, age</h2>
                                         <img className="profile-info-container image profile-picture overlay gradient-group name-connected-group connected" src={connected} alt="User connected" />
                                     </div>
-                                    <h3>last seen 10min ago</h3>
-                                    <h3>165m away</h3>
+                                    <h2>last seen 10min ago</h2>
+                                    <h2>165m away</h2>
                                 </div>
                                 <div className="profile-info-container image profile-picture overlay gradient-group favorite-blocked-group">
                                     <img src={notfavorite} alt="User not added to favorites" />
@@ -45,11 +45,43 @@ const ProfileInfo = () => {
             </div>
             <div className="profile-info-container bottom">
                 <div className="profile-info-container bottom info">
-                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor praesentium, cum nesciunt molestias, ipsam tempora laudantium libero ut perferendis mollitia officia voluptas nulla vel eaque ex quia doloribus, deserunt dicta?
-                    Delectus vel fugiat saepe sed, nemo ipsa architecto! Omnis, tempora iste dignissimos vero laboriosam dicta vel ullam blanditiis atque fugit enim adipisci quo alias magni expedita velit, aperiam officiis ab?
-                    Optio veniam atque numquam rem maiores. Ipsam, vero blanditiis mollitia nulla sapiente laboriosam, nam sint atque, nobis facere incidunt? Nobis ipsa minus molestias, aperiam deleniti aliquid voluptatem excepturi culpa iure!</div>
-                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptate natus voluptatum, deleniti autem nobis nemo, tempora molestias doloribus eos et eum ea unde earum consectetur eligendi laboriosam magnam eaque.</div>
-                    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias earum labore quaerat officia ratione corrupti a aperiam, vel obcaecati expedita illum recusandae error similique nostrum possimus quod repellendus ipsam itaque!</div>
+                    <div className="profile-info-container bottom info description">
+                        <h4>Description</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eaque tempora dignissimos quo omnis, est id ut esse quia nihil quis architecto amet iure repudiandae maiores mollitia saepe corrupti accusantium!</p>
+                    </div>
+                    <div className="profile-info-container bottom info interests">
+                        <h4>What are my interests?</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptate natus voluptatum, deleniti autem nobis nemo, tempora molestias doloribus eos et eum ea unde earum consectetur eligendi laboriosam magnam eaque.</p>
+                    </div>
+                    <div className="profile-info-container bottom info table">
+                        <h4>Information</h4>
+                        <div className="profile-info-container bottom info table-details">
+                            <div className="profile-info-container bottom info table-details row height">
+                                <h5>Height</h5>
+                                <span>165cm</span>
+                            </div>
+                            <div className="profile-info-container bottom info table-details row weight">
+                                <h5>Weight</h5>
+                                <span>56kg</span>
+                            </div>
+                            <div className="profile-info-container bottom info table-details row ethnicity">
+                                <h5>Ethnicity</h5>
+                                <span>Latina</span>
+                            </div>
+                            <div className="profile-info-container bottom info table-details row gender">
+                                <h5>Gender</h5>
+                                <span>Cis woman</span>
+                            </div>
+                            <div className="profile-info-container bottom info table-details row pronouns">
+                                <h5>Pronouns</h5>
+                                <span>She/They</span>
+                            </div>
+                            <div className="profile-info-container bottom info table-details row lookingfor">
+                                <h5>Looking for</h5>
+                                <span>Relationships</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
