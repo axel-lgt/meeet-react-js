9968280d4a22e762c05318c32a0e045f767267ea
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-03-21 11:14:25 +0100

    fixed overlay with absolute positions and some top/bottom tweaking

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -15,36 +15,42 @@ const ProfileInfo = () => {
         <div className="profile-info-container">
             <div className="profile-info-container image">
                 <div className="profile-info-container image profile-picture">
-                    <img src={imgplaceholder} alt="User avatar" />
-                </div>
-                <div className="profile-info-container overlay">
-                    <div className="profile-info-container overlay gradient">
-                        <div className="profile-info-container overlay gradient-group">
-                            <div className="profile-info-container overlay gradient-group back-arrow">
-                                <img src={backarrow} alt="Return to last page" />
-                            </div>
-                            <div className="profile-info-container overlay gradient-group name-connected-distance">
-                                <div className="profile-info-container overlay gradient-group name-connected-distance name-connected-group">
-                                     <h3>Name, age</h3>
-                                    <img className="profile-info-container overlay gradient-group name-connected-group connected" src={connected} alt="User connected" />
+                    <img className="profile-info-container image profile-picture main-photo" src={imgplaceholder} alt="User avatar" />
+                    <div className="profile-info-container image profile-picture overlay">
+                        <div className="profile-info-container image profile-picture overlay gradient">
+                            <div className="profile-info-container image profile-picture overlay gradient-group">
+                                <div className="profile-info-container image profile-picture overlay gradient-group back-arrow">
+                                    <img src={backarrow} alt="Return to last page" />
                                 </div>
-                                <h3>last seen 10min ago</h3>
-                                <h3>165m away</h3>
-                            </div>
-                            <div className="profile-info-container overlay gradient-group favorite-blocked-group">
-                                <img src={notfavorite} alt="User not added to favorites" />
-                                <img src={block} alt="Block user" />
+                                <div className="profile-info-container image profile-picture overlay gradient-group name-connected-distance">
+                                    <div className="profile-info-container image profile-picture overlay gradient-group name-connected-distance name-connected-group">
+                                        <h3>Name, age</h3>
+                                        <img className="profile-info-container image profile-picture overlay gradient-group name-connected-group connected" src={connected} alt="User connected" />
+                                    </div>
+                                    <h3>last seen 10min ago</h3>
+                                    <h3>165m away</h3>
+                                </div>
+                                <div className="profile-info-container image profile-picture overlay gradient-group favorite-blocked-group">
+                                    <img src={notfavorite} alt="User not added to favorites" />
+                                    <img src={block} alt="Block user" />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="profile-info-container overlay bottom-group">
-                        <img src={dots} alt="Amount of pictures" />
-                        <img src={message} alt="Message this user" />
+                        <div className="profile-info-container image profile-picture overlay bottom-group">
+                            <img className="profile-info-container image profile-picture overlay bottom-group dots" src={dots} alt="Amount of pictures" />
+                            <img className="profile-info-container image profile-picture overlay bottom-group message" src={message} alt="Message this user" />
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="profile-info-container bottom">
-                <div className="profile-info-container bottom info"></div>
+                <div className="profile-info-container bottom info">
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor praesentium, cum nesciunt molestias, ipsam tempora laudantium libero ut perferendis mollitia officia voluptas nulla vel eaque ex quia doloribus, deserunt dicta?
+                    Delectus vel fugiat saepe sed, nemo ipsa architecto! Omnis, tempora iste dignissimos vero laboriosam dicta vel ullam blanditiis atque fugit enim adipisci quo alias magni expedita velit, aperiam officiis ab?
+                    Optio veniam atque numquam rem maiores. Ipsam, vero blanditiis mollitia nulla sapiente laboriosam, nam sint atque, nobis facere incidunt? Nobis ipsa minus molestias, aperiam deleniti aliquid voluptatem excepturi culpa iure!</div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptate natus voluptatum, deleniti autem nobis nemo, tempora molestias doloribus eos et eum ea unde earum consectetur eligendi laboriosam magnam eaque.</div>
+                    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias earum labore quaerat officia ratione corrupti a aperiam, vel obcaecati expedita illum recusandae error similique nostrum possimus quod repellendus ipsam itaque!</div>
+                </div>
             </div>
         </div>
     )
